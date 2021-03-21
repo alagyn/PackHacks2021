7484d55cb4d7a56297cddd69a545579a53b6f07e
Ryan <rnrice2@gmail.com>
2021-03-21 04:26:37 -0400

    Updated README.md and dataGrapher package

--- core/dataGrapher/parseDebtByLocation.py
+++ core/dataGrapher/parseDebtByLocation.py
@@ -7,10 +7,15 @@ sheets = workbook.sheets()
 x: xlrd.sheet.Sheet = sheets[0]
 place = []
 balance = []
+
+# Loop through the data range
 for row in range(7, 59):
     place.append(x[row][0])
     balance.append(x[row][1].value)
-    # Remove Puerto Rico from the dataset.
+
+# Remove Puerto Rico from the dataset.
 place.pop(38)
 balance.pop(38)
+
+# Create a map from the data
 InteractiveMapUnitedStates.create_map(balance)
